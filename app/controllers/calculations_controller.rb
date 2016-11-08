40e8746ce7608ae461bbffcaa63ba7b8ad544bb5
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-11-07 23:26:33 -0600

    Final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,33 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number = params[:id].to_i
+    @square = @number**2
+  end
+
+  def square_root
+    @number = params[:id].to_i
+    @squareroot = (@number**(0.5)).round(2)
+  end
+
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random = rand(@min..@max)
+  end
+
+  def payment
+    @aprbasis = params[:ir].to_f
+    @years = params[:noy].to_i
+    @principal = params[:pv].to_i
+
+    @apr = @aprbasis/100
+
+    @rate = @apr/12/100
+    @periods = @years*12
+
+    @payment = '%.2f' % [@principal*(@rate/(1-(1+@rate)**(-@periods)))]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+
+  get("/payment/:ir/:noy/:pv", { :controller => "calculations", :action => "payment" })
 end
